c73c53cbdb92c377f991e25cd5183a284d6bb162
audreysc <audreyscopeland@gmail.com>
2015-09-02 23:58:54 -0400

    Reroute '/' to '/plot'

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ from plotly.graph_objs import *
 import data as d
 import places as p
 from collections import OrderedDict
-#import json
+import json
 
+# For development
 # grab username and key from config/data file
 #with open('data.json') as config_file:
 #    config_data = json.load(config_file)
@@ -16,11 +17,17 @@ from collections import OrderedDict
 #key = config_data["key"]
 #racery_base_url = config_data["url"]
 
+# For production
 username = os.environ["PLOTLY_USER"]
 key = os.environ["PLOTLY_KEY"]
-#racery_base_url = os.environ["BASE_URL"]
+
+# Plotly signin
 py.sign_in(username, key)
-# add your username and api key
+
+
+@get('/')
+def wrong():
+    redirect("/plot")
 
 @get('/plot')
 def form():
@@ -32,15 +39,6 @@ def submit():
     # grab data from form
     ID = request.forms.get('ID')
     NAME = request.forms.get('NAME')
-    #all_runners = d.start_graph_race(ID)
-    #plot_url_place = p.graph_ranks_runner(all_runners, NAME)
-    ## TODO: plotgaps should return a data object instead of creating that here
-    #graph = d.plotgaps(ID, NAME)
-    #trace = graph[0]
-    #layout = graph[1]
-    #data=Data([trace])
-    #fig = Figure(data=data,layout=layout)
-    #plot_url_gaps = py.plot(fig, filename='gaps_hc', auto_open=False)
     urls = d.start_graph_race_name(ID,NAME)
     plot_url_place = urls[0]
     plot_url_gaps = urls[1]
